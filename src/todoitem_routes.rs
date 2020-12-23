42f2cde0028476f7014ec2aefe1ab60238ab47fe
Dominik Facko <dominik.facko@barrage.net>
2020-12-23 14:28:04 +0100

    adding user checks on all routes interacting with items/lists

--- src/todoitem_routes.rs
+++ src/todoitem_routes.rs
@@ -4,7 +4,11 @@ use super::models::*;
 use actix_web::{web, HttpResponse, Responder};
 use serde_json::*;
 
-pub async fn additem(conn: web::Data<Pool>, newitem: web::Json<Value>) -> impl Responder {
+pub async fn additem(
+    conn: web::Data<Pool>,
+    newitem: web::Json<Value>,
+    req: web::HttpRequest,
+) -> impl Responder {
     let data: String = match json_validation::validate(
         &newitem,
         vec!["finished|bool", "list_id|int", "task|string"],
@@ -12,21 +16,67 @@ pub async fn additem(conn: web::Data<Pool>, newitem: web::Json<Value>) -> impl R
         Some(error) => return Ok(HttpResponse::UnprocessableEntity().json(error)),
         None => to_string(&newitem.into_inner()).unwrap(),
     };
-    println!("this is data {:?}", data);
     let insertable_item: TodoItemNew = serde_json::from_str(&data).unwrap();
-    println!("Created todo item");
-    TodoItem::create_item(&conn.get().unwrap(), insertable_item)
-        .map(|item| HttpResponse::Ok().json(item))
-        .map_err(|_| HttpResponse::InternalServerError().finish())
+    let user = req.extensions_mut().remove::<User>().unwrap();
+    match User::user_lists(&conn.get().unwrap(), user) {
+        Ok(lists) => {
+            for list in lists {
+                if list.id == insertable_item.list_id {
+                    return TodoItem::create_item(&conn.get().unwrap(), insertable_item)
+                        .map(|item| HttpResponse::Ok().json(item))
+                        .map_err(|_| {
+                            HttpResponse::InternalServerError().json(json!({
+                                "message":"failed to insert into list"
+                            }))
+                        });
+                }
+            }
+            return Ok(HttpResponse::Unauthorized().json(json!({
+                "message":"Cannot edit that list"
+            })));
+        }
+
+        Err(_) => {
+            return Ok(HttpResponse::InternalServerError().json(json!({
+                "message":"failed to get user lists"
+            })))
+        }
+    };
 }
 
-pub async fn items_from_list(conn: web::Data<Pool>, list_id: web::Path<i32>) -> impl Responder {
-    TodoItem::items_from_list(&conn.get().unwrap(), list_id.into_inner())
-        .map(|items| HttpResponse::Ok().json(items))
-        .map_err(|_| HttpResponse::NotFound().finish())
+pub async fn items_from_list(
+    conn: web::Data<Pool>,
+    list_id: web::Path<i32>,
+    req: web::HttpRequest,
+) -> impl Responder {
+    let user = req.extensions_mut().remove::<User>().unwrap();
+    match User::user_lists(&conn.get().unwrap(), user.clone()) {
+        Ok(lists) => {
+            for list in lists {
+                if list.id == list_id.clone() && list.user_id == user.id {
+                    return TodoItem::items_from_list(&conn.get().unwrap(), list_id.into_inner())
+                        .map(|items| HttpResponse::Ok().json(items))
+                        .map_err(|_| HttpResponse::NotFound().finish());
+                }
+            }
+            return Ok(HttpResponse::Unauthorized().json(json!({
+                "message":"Cannot edit that list"
+            })));
+        }
+
+        Err(_) => {
+            return Ok(HttpResponse::InternalServerError().json(json!({
+                "message":"failed to get user lists"
+            })))
+        }
+    };
 }
 
-pub async fn delete_item(conn: web::Data<Pool>, item_id: web::Path<i32>) -> impl Responder {
+pub async fn delete_item(
+    conn: web::Data<Pool>,
+    item_id: web::Path<i32>,
+    req: web::HttpRequest,
+) -> impl Responder {
     TodoItem::delete_item(&conn.get().unwrap(), item_id.into_inner())
         .map(|item| {
             HttpResponse::Ok().json(json!({
@@ -36,13 +86,21 @@ pub async fn delete_item(conn: web::Data<Pool>, item_id: web::Path<i32>) -> impl
         .map_err(|_| HttpResponse::NotFound().finish())
 }
 
-pub async fn check_item(conn: web::Data<Pool>, item_id: web::Path<i32>) -> impl Responder {
+pub async fn check_item(
+    conn: web::Data<Pool>,
+    item_id: web::Path<i32>,
+    req: web::HttpRequest,
+) -> impl Responder {
     TodoItem::check(&conn.get().unwrap(), item_id.into_inner())
         .map(|item| HttpResponse::Ok().json(json!({ "updated item": item })))
         .map_err(|_| HttpResponse::InternalServerError().finish())
 }
 
-pub async fn uncheck_item(conn: web::Data<Pool>, item_id: web::Path<i32>) -> impl Responder {
+pub async fn uncheck_item(
+    conn: web::Data<Pool>,
+    item_id: web::Path<i32>,
+    req: web::HttpRequest,
+) -> impl Responder {
     TodoItem::uncheck(&conn.get().unwrap(), item_id.into_inner())
         .map(|item| HttpResponse::Ok().json(json!({ "updated item": item })))
         .map_err(|_| HttpResponse::InternalServerError().finish())
--- src/todolist_routes.rs
+++ src/todolist_routes.rs
@@ -2,6 +2,7 @@ use super::db::*;
 use super::models::*;
 use actix_web::http::StatusCode;
 use actix_web::{web, HttpResponse, Responder};
+use serde_json::json;
 
 pub async fn lists(conn: web::Data<Pool> /*req: web::HttpRequest*/) -> impl Responder {
     let lists = TodoList::get_all_lists(&conn.get().unwrap()).await;
@@ -19,21 +20,21 @@ pub async fn lists(conn: web::Data<Pool> /*req: web::HttpRequest*/) -> impl Resp
 #[derive(Debug, serde::Deserialize)]
 pub struct TodoListJson {
     pub title: Option<String>,
-    pub user_id: Option<i32>,
 }
 
-pub async fn addlist(conn: web::Data<Pool>, newlist: web::Json<TodoListJson>) -> impl Responder {
+pub async fn addlist(
+    conn: web::Data<Pool>,
+    newlist: web::Json<TodoListJson>,
+    req: web::HttpRequest,
+) -> impl Responder {
     let title: String = match &newlist.title {
         Some(title) => title.to_string(),
         None => return Ok(HttpResponse::BadRequest().finish()),
     };
-    let user_id: i32 = match &newlist.user_id {
-        Some(id) => *id,
-        None => return Ok(HttpResponse::BadRequest().finish()),
-    };
+    let user = req.extensions_mut().remove::<User>().unwrap();
     let newlist = TodoListNew {
         title: &title,
-        user_id: user_id,
+        user_id: user.id,
     };
 
     TodoList::create_list(&conn.get().unwrap(), newlist)
@@ -56,9 +57,31 @@ pub async fn list_by_id(conn: web::Data<Pool>, path: web::Path<i32>) -> impl Res
         .map_err(|_| HttpResponse::InternalServerError().finish())
 }
 
-pub async fn delete_list(conn: web::Data<Pool>, path: web::Path<i32>) -> impl Responder {
-    println!("Deleting list");
+pub async fn delete_list(
+    conn: web::Data<Pool>,
+    path: web::Path<i32>,
+    req: web::HttpRequest,
+) -> impl Responder {
+    let user = req.extensions_mut().remove::<User>().unwrap();
+    println!("got user");
+    match TodoList::get_list_by_id(&conn.get().unwrap(), path.clone()) {
+        Ok(list) => {
+            if list.id != user.id {
+                return Ok(HttpResponse::Ok().json(json!({
+                    "message":"cannot delete"
+                })));
+            }
+        }
+        Err(_) => {
+            println!("no list found");
+            return Ok(HttpResponse::InternalServerError().finish());
+        }
+    };
     TodoList::delete_list(&conn.get().unwrap(), path.into_inner())
-        .map(|item| HttpResponse::Ok().body(format!("{} item has been sucessfully removed ", item)))
+        .map(|item| {
+            HttpResponse::Ok().json(json!({
+                "message": format!("{} item has been removed", item)
+            }))
+        })
         .map_err(|_| HttpResponse::InternalServerError().finish())
 }
